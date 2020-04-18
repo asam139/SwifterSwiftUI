921d015e461d3d08b1224f2c83188b6bdb4ba758
Saul Moreno Abril <93sauu@gmail.com>
2020-04-18 12:09:09 +0200

    Add comments

--- Sources/SwifterSwiftUI/Utils/IfLet.swift
+++ Sources/SwifterSwiftUI/Utils/IfLet.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+/// SwifterSwiftUI: it is an utility that adds back a way to use the if let.
+///
+///    ifLet(myImage, then: {
+///       $0.resizable()
+///    })
+///
+/// - Parameters:
+///   - value: optional value to verify
+///   - then: callback to create a view with the value is not nil
+/// - Returns: The built view
 public func ifLet<T, ThenOut: View>(_ value: T?, then: (T) -> ThenOut) -> some View {
     ViewBuilder.buildIf(value.map { then($0) })
 }
 
+/// SwifterSwiftUI: it is an utility that adds back a way to use the if let with an else option
+///
+///    ifLet(myImage, then: {
+///       $0.resizable()
+///    }, else: {
+///       Text("Hello")
+///    })
+///
+/// - Parameters:
+///   - value: optional value to verify
+///   - then: callback to create a view with the value is not nil
+/// - Returns: The built view
 public func ifLet<T, ThenOut: View, ElseOut: View>(_ value: T?, then: (T) -> ThenOut, `else`: () -> ElseOut) -> some View {
     value.map { ViewBuilder.buildEither(first: then($0)) } ?? ViewBuilder.buildEither(second: `else`())
 }
--- Sources/SwifterSwiftUI/View+Extensions.swift
+++ Sources/SwifterSwiftUI/View+Extensions.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 // MARK: General
 extension View {
+
+    /// SwifterSwiftUI: Type casting to AnyView
+    ///
+    ///    myView.eraseToAnyView()
+    ///
+    /// - Returns: View as AnyView
     public func eraseToAnyView() -> AnyView {
         AnyView(self)
     }
@@ -16,6 +22,18 @@ extension View {
 
 // MARK: Building
 extension View {
+
+    /// SwifterSwiftUI: Apply changes to the view if the condition is true
+    ///
+    ///    myView
+    ///    .if(index == state.currentIndex, then: {
+    ///        $0.background(Color.red)
+    ///    })
+    ///
+    /// - Parameters:
+    ///   - condition: an boolean to control the condition
+    ///   - then: callback to apply the changes when the condition is true
+    /// - Returns: some View
     public func `if`<Content: View>(_ conditional: Bool,
                                     then: (Self) -> Content) -> some View {
         if conditional {
@@ -25,6 +43,20 @@ extension View {
         }
     }
 
+    /// SwifterSwiftUI: Apply some changes to the view in place of the condition
+    ///
+    ///    myView
+    ///    .if(index == state.currentIndex, then: {
+    ///        $0.background(Color.red)
+    ///    }, else: {
+    ///        $0.background(Color.yellow)
+    ///    })
+    ///
+    /// - Parameters:
+    ///   - condition: an boolean to control the condition
+    ///   - then: callback to apply the changes when the condition is true
+    ///   - else: callback to apply the changes when the condition is false
+    /// - Returns: some View
     public func `if`<ThenView: View, ElseView: View>(
         _ conditional: Bool,
         then: (Self) -> ThenView,
@@ -39,6 +71,15 @@ extension View {
 
 // MARK: Modifiers
 extension View {
+
+    /// SwifterSwiftUI: Set one modifier conditionally.
+    ///
+    ///    myView.conditionalModifier(myCondition, myViewModifier)
+    ///
+    /// - Parameters:
+    ///   - condition: an boolean to control the condition
+    ///   - modifier: modifier to apply
+    /// - Returns: some View
     public func conditionalModifier<T>(_ condition: Bool, _ modifier: T) -> some View where T: ViewModifier {
         Group {
             if condition {
@@ -49,6 +90,15 @@ extension View {
         }
     }
 
+    /// SwifterSwiftUI: Set one modifier or another conditionally.
+    ///
+    ///    myView.conditionalModifier(myCondition, firstViewModifier, secondViewModifier)
+    ///
+    /// - Parameters:
+    ///   - condition: an boolean to control the condition
+    ///   - trueModifier: modifier to apply when the condition is true
+    ///   - falseModifier: modifier to apply when the condition is false
+    /// - Returns: some View
     public func conditionalModifier<M1, M2>(
         _ condition: Bool, _ trueModifier: M1, _ falseModifier: M2
     ) -> some View where M1: ViewModifier, M2: ViewModifier {
@@ -64,6 +114,15 @@ extension View {
 
 // MARK: Animations
 extension View {
+
+    /// SwifterSwiftUI: Animate an action with an animation on appear.
+    ///
+    ///    myView.animate(using: .easeInOut) { self.scale = 0.5 }
+    ///
+    /// - Parameters:
+    ///   - animation: animation to be applied
+    ///   - action: action to be animated
+    /// - Returns: some View
     public func animate(using animation: Animation = Animation.easeInOut(duration: 1),
                         _ action: @escaping () -> Void) -> some View {
         return onAppear {
@@ -73,6 +132,15 @@ extension View {
         }
     }
 
+    /// SwifterSwiftUI: Animate an action with an inifinite animation on appear.
+    ///
+    ///    myView.animateForever(autoreverses: true) { self.scale = 0.5 }
+    ///
+    /// - Parameters:
+    ///   - animation: animation to be applied
+    ///   - autoreverses: it will do the initial animation but in reverse
+    ///   - action: action to be animated
+    /// - Returns: some View
     public func animateForever(using animation: Animation = Animation.easeInOut(duration: 1),
                                autoreverses: Bool = false, _ action: @escaping () -> Void) -> some View {
         let repeated = animation.repeatForever(autoreverses: autoreverses)
