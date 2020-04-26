b1baac545e1bca9c62fce5153997ce70e89063a7
Saul Moreno Abril <93sauu@gmail.com>
2020-04-26 19:05:49 +0200

    Uncommented code

--- Tests/SwifterSwiftUITests/SwifterSwiftUI/Image+ExtensionsTests.swift
+++ Tests/SwifterSwiftUITests/SwifterSwiftUI/Image+ExtensionsTests.swift
@@ -7,18 +7,20 @@
 
 import XCTest
 import SwiftUI
+import ViewInspector
 @testable import SwifterSwiftUI
 
-//final class ImageExtensionsTests: XCTestCase {
-//    func testInitWithNameAndDefaultImage() {
-//
-//        let image = Image("photo", defaultImage: "empty-photo")
-//
-//    }
-//
-//    #if canImport(UIKit)
-//    func testInitWithNameAndDefaultSystemImage() {
-//        let image = Image("photo", defaultSystemImage: "bandage.fill")
-//    }
-//    #endif
-//}
+final class ImageExtensionsTests: XCTestCase {
+    func testInitWithNameAndDefaultImage() {
+
+        let image = Image("photo", defaultImage: "empty-photo")
+        XCTAssertNoThrow(try image.inspect().image())
+    }
+
+    #if canImport(UIKit)
+    func testInitWithNameAndDefaultSystemImage() {
+        let image = Image("photo", defaultSystemImage: "bandage.fill")
+        XCTAssertNoThrow(try image.inspect().image())
+    }
+    #endif
+}
