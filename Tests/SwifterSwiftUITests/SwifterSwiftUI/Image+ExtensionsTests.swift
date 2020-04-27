9b0d38edbf10c8942db28b67571211368fc97fa6
Saul Moreno Abril <93sauu@gmail.com>
2020-04-27 19:52:31 +0200

    Add TODO to implement test case when resources are supported.

--- Tests/SwifterSwiftUITests/SwifterSwiftUI/Image+ExtensionsTests.swift
+++ Tests/SwifterSwiftUITests/SwifterSwiftUI/Image+ExtensionsTests.swift
@@ -12,7 +12,8 @@ import ViewInspector
 
 final class ImageExtensionsTests: XCTestCase {
     func testInitWithNameAndDefaultImage() {
-
+        // TODO: testing success image when Swift 5.3 is available
+        // Ref: https://github.com/apple/swift-evolution/blob/master/proposals/0271-package-manager-resources.md
         let defaultImage = Image("no-example", defaultImage: "empty-photo")
         XCTAssertNoThrow(try defaultImage.inspect().image())
 
